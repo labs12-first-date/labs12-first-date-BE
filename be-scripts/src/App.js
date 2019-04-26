e8198e15c5071ba95b372dbad6500d4cddc1adfe
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-04-26 13:31:12 -0400

    App

--- be-scripts/src/App.js
+++ be-scripts/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Profiles from './components/ProfileSeeds';
+import Seeds from './components/UserSeeds';
 
 function App() {
   return (
     <div className="App">
-      
+      <Profiles />
+      <Seeds />
     </div>
   );
 }
