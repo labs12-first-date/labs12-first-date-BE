b9b1a65de73aedf7677a321750cc08d68ec21c52
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-04-30 14:15:12 -0400

    DOB

--- be-scripts/src/components/onboarding.jsx
+++ be-scripts/src/components/onboarding.jsx
@@ -11,10 +11,24 @@ export default function OnBoarding() {
       onboardingStep: 1
     });
 
+    fb.firestore().collection('onboarding').doc('Gl4NcAPLec4gUkDtGwaV').set({
+      prompt: "What is your last name?",
+      inputPlaceholder: "Last Name",
+      fieldName: 'lastName',
+      onboardingStep: 1
+    });
+
     fb.firestore().collection('onboarding').doc('plzR2n89ekKPnxLCGSyP').set({
       prompt: "What is your gender?",
       inputPlaceholder: "Gender",
       fieldName: 'gender',
+      onboardingStep: 2
+    });
+
+    fb.firestore().collection('onboarding').doc('ZCRBzXVsPwBt0GlorgHj').set({
+      prompt: "When's your birthday?",
+      inputPlaceholder: "MM/DD/YYYY",
+      fieldName: 'dateOfBirth',
       onboardingStep: 2
     });
   
